ced460f006943041a9e3540a3bf5abb40e0c3244
Daniël de Kok <me@danieldk.eu>
2018-06-10 08:36:03 +0200

    Performance improvements for scaled adds.
    - Avoid a branch point by hinting to the compiler that the vectors are
      the same length.
    - Avoid multiplications when the scaling factor is 1.0.

--- finalfrontier/src/vec_simd.rs
+++ finalfrontier/src/vec_simd.rs
@@ -41,20 +41,33 @@ cfg_if! {
 fn scaled_add_f32x4(mut u: ArrayViewMut1<f32>, v: ArrayView1<f32>, a: f32) {
     assert_eq!(u.len(), v.len());
 
-    let mut u = u.as_slice_mut()
+    let mut u = u
+        .as_slice_mut()
         .expect("Cannot apply SIMD instructions on non-contiguous data.");
-    let mut v = v.as_slice()
-        .expect("Cannot apply SIMD instructions on non-contiguous data.");
+    let mut v = &v
+        .as_slice()
+        .expect("Cannot apply SIMD instructions on non-contiguous data.")[..u.len()];
 
-    let ax4 = f32x4::splat(a);
+    if a == 1f32 {
+        while u.len() >= 4 {
+            let mut ux4 = f32x4::load_unaligned(u);
+            let vx4 = f32x4::load_unaligned(v);
+            ux4 += vx4;
+            ux4.store_unaligned(u);
+            u = &mut { u }[4..];
+            v = &v[4..];
+        }
+    } else {
+        let ax4 = f32x4::splat(a);
 
-    while u.len() >= 4 {
-        let mut ux4 = f32x4::load_unaligned(u);
-        let vx4 = f32x4::load_unaligned(v);
-        ux4 += vx4 * ax4;
-        ux4.store_unaligned(u);
-        u = &mut { u }[4..];
-        v = &v[4..];
+        while u.len() >= 4 {
+            let mut ux4 = f32x4::load_unaligned(u);
+            let vx4 = f32x4::load_unaligned(v);
+            ux4 += vx4 * ax4;
+            ux4.store_unaligned(u);
+            u = &mut { u }[4..];
+            v = &v[4..];
+        }
     }
 
     scaled_add_unvectorized(u, v, a);
@@ -64,20 +77,33 @@ fn scaled_add_f32x4(mut u: ArrayViewMut1<f32>, v: ArrayView1<f32>, a: f32) {
 fn scaled_add_f32x8(mut u: ArrayViewMut1<f32>, v: ArrayView1<f32>, a: f32) {
     assert_eq!(u.len(), v.len());
 
-    let mut u = u.as_slice_mut()
+    let mut u = u
+        .as_slice_mut()
         .expect("Cannot apply SIMD instructions on non-contiguous data.");
-    let mut v = v.as_slice()
-        .expect("Cannot apply SIMD instructions on non-contiguous data.");
+    let mut v = &v
+        .as_slice()
+        .expect("Cannot apply SIMD instructions on non-contiguous data.")[..u.len()];
 
-    let ax8 = f32x8::splat(a);
+    if a == 1f32 {
+        while u.len() >= 8 {
+            let mut ux8 = f32x8::load_unaligned(u);
+            let vx8 = f32x8::load_unaligned(v);
+            ux8 += vx8;
+            ux8.store_unaligned(u);
+            u = &mut { u }[8..];
+            v = &v[8..];
+        }
+    } else {
+        let ax8 = f32x8::splat(a);
 
-    while u.len() >= 8 {
-        let mut ux8 = f32x8::load_unaligned(u);
-        let vx8 = f32x8::load_unaligned(v);
-        ux8 += vx8 * ax8;
-        ux8.store_unaligned(u);
-        u = &mut { u }[8..];
-        v = &v[8..];
+        while u.len() >= 8 {
+            let mut ux8 = f32x8::load_unaligned(u);
+            let vx8 = f32x8::load_unaligned(v);
+            ux8 += vx8 * ax8;
+            ux8.store_unaligned(u);
+            u = &mut { u }[8..];
+            v = &v[8..];
+        }
     }
 
     scaled_add_unvectorized(u, v, a);
@@ -86,14 +112,21 @@ fn scaled_add_f32x8(mut u: ArrayViewMut1<f32>, v: ArrayView1<f32>, a: f32) {
 fn scaled_add_unvectorized(u: &mut [f32], v: &[f32], a: f32) {
     assert_eq!(u.len(), v.len());
 
-    for i in 0..u.len() {
-        u[i] += v[i] * a;
+    if a == 1f32 {
+        for i in 0..u.len() {
+            u[i] += v[i];
+        }
+    } else {
+        for i in 0..u.len() {
+            u[i] += v[i] * a;
+        }
     }
 }
 
 #[allow(dead_code)]
 fn scale_f32x4(mut u: ArrayViewMut1<f32>, a: f32) {
-    let mut u = u.as_slice_mut()
+    let mut u = u
+        .as_slice_mut()
         .expect("Cannot apply SIMD instructions on non-contiguous data.");
 
     let ax4 = f32x4::splat(a);
@@ -143,6 +176,35 @@ mod tests {
 
     #[cfg(feature = "avx-accel")]
     use super::{scale_f32x8, scaled_add_f32x8};
+
+    #[test]
+    fn add_unvectorized_test() {
+        let u = &mut [1., 2., 3., 4., 5.];
+        let v = &[5., 3., 3., 2., 1.];
+        scaled_add_unvectorized(u, v, 1.0);
+        assert!(all_close(u, &[6.0, 5.0, 6.0, 6.0, 6.0], 1e-5));
+    }
+
+    #[test]
+    fn add_f32x4_test() {
+        let mut u = Array1::random((102,), Range::new(-1.0, 1.0));
+        let v = Array1::random((102,), Range::new(-1.0, 1.0));
+        let mut check = u.clone();
+        scaled_add_unvectorized(check.as_slice_mut().unwrap(), v.as_slice().unwrap(), 1.0);
+        scaled_add_f32x4(u.view_mut(), v.view(), 1.0);
+        assert!(array_all_close(check.view(), u.view(), 1e-5));
+    }
+
+    #[test]
+    #[cfg(feature = "avx-accel")]
+    fn add_f32x8_test() {
+        let mut u = Array1::random((102,), Range::new(-1.0, 1.0));
+        let v = Array1::random((102,), Range::new(-1.0, 1.0));
+        let mut check = u.clone();
+        scaled_add_unvectorized(check.as_slice_mut().unwrap(), v.as_slice().unwrap(), 1.0);
+        scaled_add_f32x8(u.view_mut(), v.view(), 1.0);
+        assert!(array_all_close(check.view(), u.view(), 1e-5));
+    }
 
     #[test]
     fn scaled_add_unvectorized_test() {
