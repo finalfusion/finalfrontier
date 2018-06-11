b78e05520fdf11f98ee34f72cb6e214cf077fa35
Daniël de Kok <me@danieldk.eu>
2018-06-11 08:27:50 +0200

    Add a generator that draws integers following a Zipfian distribution.
    Although the WeightedRangeGenerator works well for selecting negative
    samples, more than 1/5th of the training time is spend in this
    iterator with large vocabulary sizes. Even though the number of
    comparisons in the WeightedRangeGenerator is relatively small (due to
    binary search), the prefix sum array does not fit CPU cache well,
    and becomes memory latency-bound.
    
    This generator approximates the token frequencies using a Zipfian
    distribution with exponent 1. This generator barely shows up in the
    profiles and makes training a fair bit faster.
    
    Future work: the fastText paper suggests using the square root of a
    token's frequency as the weight in negative sampling. Using this
    Zipfian distribution gives a larger weight to more frequent
    tokens. This may need more tweaking in the future.

--- finalfrontier/src/lib.rs
+++ finalfrontier/src/lib.rs
@@ -19,11 +19,16 @@ extern crate ndarray_rand;
 
 extern crate rand;
 
+extern crate zipf;
+
 mod io;
 pub use io::SentenceIterator;
 
 mod config;
 pub use config::{Config, LossType, ModelType};
+
+mod sampling;
+pub use sampling::{WeightedRangeGenerator, ZipfRangeGenerator};
 
 mod subword;
 pub use subword::{NGrams, SubwordIndices};
