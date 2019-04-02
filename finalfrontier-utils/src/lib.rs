b78c06029a8331d13b83c1d9eb974696b382987b
Sebastian Puetz <sebastian.puetz@student.uni-tuebingen.de>
2019-04-02 21:46:41 +0200

    Refactor finalfrontier-utils.

--- finalfrontier-utils/src/lib.rs
+++ finalfrontier-utils/src/lib.rs
@@ -3,3 +3,9 @@ pub use crate::data::thread_data;
 
 mod progress;
 pub use crate::progress::FileProgress;
+
+mod util;
+pub use crate::util::{
+    common_config_from_matches, show_progress, skipgram_config_from_matches,
+    subword_config_from_matches, AppBuilder,
+};
