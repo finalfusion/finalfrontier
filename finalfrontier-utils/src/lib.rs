d4f6813dcba9579b5d1188670f84775e58410dd9
Sebastian Puetz <sebastian.puetz@student.uni-tuebingen.de>
2019-04-04 17:46:03 +0200

    Add AppStructs for Depembeds and SkipGram.
    Add structs holding relevant information for depembeds and
    skipgram binaries.

--- finalfrontier-utils/src/lib.rs
+++ finalfrontier-utils/src/lib.rs
@@ -5,7 +5,4 @@ mod progress;
 pub use crate::progress::FileProgress;
 
 mod util;
-pub use crate::util::{
-    common_config_from_matches, show_progress, skipgram_config_from_matches,
-    subword_config_from_matches, AppBuilder,
-};
+pub use crate::util::{show_progress, DepembedsApp, SkipGramApp};
