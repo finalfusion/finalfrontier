96240a424d906cbba43e272a67f790b274e3d2be
Sebastian Puetz <sebastian.puetz@student.uni-tuebingen.de>
2019-04-05 10:29:40 +0200

    Add binary for training dependency embeddings.
    This commit adds the final piece to enable training dependency
    embeddings.

--- finalfrontier-utils/src/bin/ff-deps.rs
+++ finalfrontier-utils/src/bin/ff-deps.rs
@@ -4,11 +4,14 @@ use std::path::{Path, PathBuf};
 use std::thread;
 use std::time::Duration;
 
+use conllx::graph::Node;
+use conllx::io::{ReadSentence, Reader};
 use finalfrontier::{
-    SentenceIterator, SkipgramTrainer, SubwordVocab, SubwordVocabConfig, Vocab, VocabBuilder,
-    WriteModelBinary, SGD,
+    DepembedsConfig, DepembedsTrainer, Dependency, DependencyIterator, SimpleVocab,
+    SimpleVocabConfig, SubwordVocab, SubwordVocabConfig, Vocab, VocabBuilder, WriteModelBinary,
+    SGD,
 };
-use finalfrontier_utils::{show_progress, thread_data_text, FileProgress, SkipGramApp};
+use finalfrontier_utils::{show_progress, thread_data_conllx, DepembedsApp, FileProgress};
 use rand::{FromEntropy, Rng};
 use rand_xorshift::XorShiftRng;
 use stdinout::OrExit;
@@ -16,19 +19,26 @@ use stdinout::OrExit;
 const PROGRESS_UPDATE_INTERVAL: u64 = 200;
 
 fn main() {
-    let app = SkipGramApp::new();
+    let app = DepembedsApp::new();
     let corpus = app.corpus();
-    let n_threads = app.n_threads();
     let common_config = app.common_config();
-    let vocab = build_vocab(app.vocab_config(), corpus);
+    let n_threads = app.n_threads();
+    let (input_vocab, output_vocab) = build_vocab(
+        app.input_vocab_config(),
+        app.output_vocab_config(),
+        app.depembeds_config(),
+        corpus,
+    );
+
     let mut output_writer = BufWriter::new(
         File::create(app.output()).or_exit("Cannot open output file for writing.", 1),
     );
-    let trainer = SkipgramTrainer::new(
-        vocab,
+    let trainer = DepembedsTrainer::new(
+        input_vocab,
+        output_vocab,
+        app.common_config(),
+        app.depembeds_config(),
         XorShiftRng::from_entropy(),
-        common_config,
-        app.skipgram_config(),
     );
     let sgd = SGD::new(trainer.into());
 
@@ -50,7 +60,7 @@ fn main() {
     }
 
     show_progress(
-        &common_config,
+        &app.common_config(),
         &sgd,
         Duration::from_millis(PROGRESS_UPDATE_INTERVAL),
     );
@@ -67,7 +77,7 @@ fn main() {
 
 fn do_work<P, R>(
     corpus_path: P,
-    mut sgd: SGD<SkipgramTrainer<R>>,
+    mut sgd: SGD<DepembedsTrainer<R>>,
     thread: usize,
     n_threads: usize,
     epochs: u32,
@@ -80,44 +90,52 @@ fn do_work<P, R>(
 
     let f = File::open(corpus_path.into()).or_exit("Cannot open corpus for reading", 1);
     let (data, start) =
-        thread_data_text(&f, thread, n_threads).or_exit("Could not get thread-specific data", 1);
+        thread_data_conllx(&f, thread, n_threads).or_exit("Could not get thread-specific data", 1);
 
-    let mut sentences = SentenceIterator::new(&data[start..]);
+    let mut sentences = Reader::new(BufReader::new(&data[start..]));
     while sgd.n_tokens_processed() < epochs as usize * n_tokens {
-        let sentence = if let Some(sentence) = sentences.next() {
+        let sentence = if let Some(sentence) = sentences
+            .read_sentence()
+            .or_exit("Could not read sentence", 1)
+        {
             sentence
         } else {
-            sentences = SentenceIterator::new(&*data);
+            sentences = Reader::new(BufReader::new(&*data));
             sentences
-                .next()
+                .read_sentence()
+                .unwrap()
                 .or_exit("Iterator does not provide sentences", 1)
-        }
-        .or_exit("Cannot read sentence", 1);
-
+        };
         let lr = (1.0 - (sgd.n_tokens_processed() as f32 / (epochs as usize * n_tokens) as f32))
             * start_lr;
-
         sgd.update_sentence(&sentence, lr);
     }
 }
 
-fn build_vocab<P>(config: SubwordVocabConfig, corpus_path: P) -> SubwordVocab
+fn build_vocab<P>(
+    input_config: SubwordVocabConfig,
+    output_config: SimpleVocabConfig,
+    dep_config: DepembedsConfig,
+    corpus_path: P,
+) -> (SubwordVocab, SimpleVocab<Dependency>)
 where
     P: AsRef<Path>,
 {
     let f = File::open(corpus_path).or_exit("Cannot open corpus for reading", 1);
     let file_progress = FileProgress::new(f).or_exit("Cannot create progress bar", 1);
+    let mut input_builder: VocabBuilder<_, String> = VocabBuilder::new(input_config);
+    let mut output_builder: VocabBuilder<_, Dependency> = VocabBuilder::new(output_config);
 
-    let sentences = SentenceIterator::new(BufReader::new(file_progress));
-
-    let mut builder: VocabBuilder<SubwordVocabConfig, String> = VocabBuilder::new(config);
-    for sentence in sentences {
+    for sentence in Reader::new(BufReader::new(file_progress)) {
         let sentence = sentence.or_exit("Cannot read sentence", 1);
+        for token in sentence.iter().filter_map(Node::token) {
+            input_builder.count(token.form());
+        }
 
-        for token in sentence {
-            builder.count(token);
+        for (_, context) in DependencyIterator::new_from_config(&sentence.dep_graph(), dep_config) {
+            output_builder.count(context);
         }
     }
 
-    builder.into()
+    (input_builder.into(), output_builder.into())
 }
