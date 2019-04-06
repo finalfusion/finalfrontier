c77f029f5634329ad27a93d49fea8222dbdd3266
Daniël de Kok <me@danieldk.eu>
2019-04-06 10:42:41 +0200

    Include man pages in release tarballs

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -4,6 +4,8 @@ set -ex
 
 cargo build --target "$TARGET" --release
 
+( cd man ; make )
+
 tmpdir="$(mktemp -d)"
 name="${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
 staging="${tmpdir}/${name}"
@@ -14,6 +16,7 @@ mkdir "${out_dir}"
 cp "target/${TARGET}/release/ff-train" "${staging}/ff-train"
 strip "${staging}/ff-train"
 cp {README.md,LICENSE,NOTICE} "${staging}/"
+cp man/*.1 "${staging}/"
 
 ( cd "${tmpdir}" && tar czf "${out_dir}/${name}.tar.gz" "${name}")
 
