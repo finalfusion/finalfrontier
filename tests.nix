c72298036bc636921a77012a057c05e8a97b7c19
Daniël de Kok <me@danieldk.eu>
2021-02-05 18:15:54 +0100

    Use cache action
    GitHub now has this handy action for caching. Simplifies things a bit
    compared to using Nix-based caching.

--- tests.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import (import ./nix/sources.nix).nixpkgs {} }:
-
-let
-  finalfrontier = import ./default.nix {
-    inherit pkgs;
-  };
-in finalfrontier.override {
-  runTests = true;
-}
